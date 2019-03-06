1e3eb6b41e799fa21a28e75a11a5e908430493b1
yun_cn <zzwyxl@163.com>
2019-03-06 16:58:11 +0800

    Auto stash before merge of "master" and "origin/master"

--- API/config.py
+++ API/config.py
@@ -1,6 +1,8 @@
 # DATABASE
 MYSQL_HOST = 'localhost'
 MYSQL_PORT = 3306
-MYSQL_USER = 'hanerx'
-MYSQL_PASSWORD = 'z1y9-KXBGHG'
+MYSQL_USER = 'root'
+MYSQL_PASSWORD = 'smhhyyz508234'
+# MYSQL_USER = 'hanerx'
+# MYSQL_PASSWORD = 'z1y9-KXBGHG'
 MYSQL_DB = 'project-agent'
